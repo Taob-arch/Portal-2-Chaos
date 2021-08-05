610336cd3e90ac83888e7f4d8b692d5e3d165a0c
Taob-arch <maneggioausten@gmail.com>
2021-08-05 23:29:44 +0100

    Added Where did everything go

---
+++ Portal 2 chaos/Effects/WDEG.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portal_2_chaos.Effects
+{
+    class WDEG : Effect
+    {
+        public WDEG() : base("Where did everything go?") { }
+
+        public override void Execute()
+        {
+            Utils.ExecuteCommand("cl_fov 180");
+
+            Utils.setTimeout(() => {
+                Utils.ExecuteCommand("cl_fov 90");
+            }, 30000);
+        }
+    }
+}
